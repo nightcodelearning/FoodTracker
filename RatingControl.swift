07e7cf613f74f4144fc91112b6558e4ce23985b7
Inés Rojas <inesrojas@192.168.1.104>
2017-05-14 20:31:51 -0500

    my first commit

---
+++ RatingControl.swift
@@ -0,0 +1,21 @@
+//
+//  RatingControl.swift
+//  foodTracker
+//
+//  Created by Inés Rojas on 5/14/17.
+//  Copyright © 2017 Inés Rojas. All rights reserved.
+//
+
+import UIKit
+
+class RatingControl: UIStackView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
