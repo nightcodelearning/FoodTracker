2744ba895dfcc1307c34d519c6add9b00d9eda8c
night code learning <nightcodelearning@gmail.com>
2017-06-08 14:36:15 -0500

    navigation, editing and delete integrated

--- Meal.swift
+++ Meal.swift
@@ -22,12 +22,10 @@ class Meal{
     init?(name: String, photo: UIImage?, rating: Int) {
         
         // The name must not be empty
-        guard !name.isEmpty else {
-            return nil
-        }
-        
+        guard !name.isEmpty,
+            (rating >= 0) && (rating <= 5)
+        else {
         // The rating must be between 0 and 5 inclusively
-        guard (rating >= 0) && (rating <= 5) else {
             return nil
         }
         
